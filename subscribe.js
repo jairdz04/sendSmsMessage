5dc4b53e691d34a01fceef6145f90533d14d3cfa
Jair Armando Diaz Zambrano <hairdz04@gmail.com>
2018-03-26 08:53:45 -0500

    .env

--- subscribe.js
+++ subscribe.js
@@ -1,27 +1,28 @@
 const soap = require("soap");
-const { MESSAGE_URL, SERVICE_KEY, SERVICE_PASSWORD }  = process.env;
+const { MESSAGE_URL, SERVICE_KEY, SERVICE_PASSWORD, SENDER, RECIEPT_REQUEST, DATA_CODING }  = process.env;
 
 exports.send = (req,res) =>{
     try {
 
-        const { subscriber, sender, message, requestId, dataCoding,receiptRequest } = req.body;
+        const { subscriber, message, requestId } = req.body;
         soap.createClient(MESSAGE_URL, (err, client) =>{
             if(err) throw new Error(err);
 
             const data = {
                 subscriber: subscriber,
-                sender: sender,
+                sender: SENDER,
                 message: message,
                 requestId: requestId,
-                receiptRequest: receiptRequest,
-                dataCoding: dataCoding
+                receiptRequest: RECIEPT_REQUEST,
+                dataCoding: DATA_CODING
             };
 
             client.setEndpoint(MESSAGE_URL);
             client.setSecurity(new soap.BasicAuthSecurity( SERVICE_KEY, SERVICE_PASSWORD));
             client.sendMessage(data, (err, result) =>{
-                if(err) return res.status(400).json(err);
-                return res.status(200).json({exito: "mensaje enviado correctamente", informacion: result});
+                if(err) return res.status(400).json({exito: false, informacion: err});
+                if(result.resultCode != "0") return res.status(200).json({exito: false , informacion: data});
+                return res.status(200).json({exito: true , informacion: result});
             });
         });
 
