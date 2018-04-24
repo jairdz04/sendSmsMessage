a3fb729f423de7e9ae714c0342e1c6cc44c3b026
Jair Armando Diaz Zambrano <hairdz04@gmail.com>
2018-04-24 10:28:58 -0500

    recieve method

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ app.use(cors({ origin: "*", optionSuccessStatus: 200 }));
 app.use(bodyParser.json());
 
 //load method to send message
-const subscribe = require("./subscribe").send;
-app.post("/send", subscribe);
-app.get("/test", (req, res)=>{
-    res.json({detalle: "esta es una prueba"})
-});
+const subscribe = require("./subscribe");
+
+app.post("/send", subscribe.send);
+//method to get message status
+app.post("/recieve",subscribe.recieve)
 
 app.listen(port, ()=>{  
     console.log("Server listening on port", port);
--- subscribe.js
+++ subscribe.js
@@ -10,19 +10,43 @@ exports.send = (req,res) =>{
 
             const data = {
                 subscriber: subscriber,
-                sender: SENDER,
+                sender: parseInt(SENDER),
                 message: message,
                 requestId: requestId,
-                receiptRequest: RECIEPT_REQUEST,
-                dataCoding: DATA_CODING
+                receiptRequest: parseInt(RECIEPT_REQUEST),
+                dataCoding: parseInt(DATA_CODING)
+            };
+            console.log(data);
+            client.setEndpoint(MESSAGE_URL);
+            client.setSecurity(new soap.BasicAuthSecurity( SERVICE_KEY, SERVICE_PASSWORD));
+            client.sendMessage(data, (err, result) =>{
+                if(err) return res.status(401).json({exito: false, informacion: err});
+                if(result.resultCode != "0") return res.status(400).json({exito: false , informacion: data});
+                return res.status(200).json({exito: true , informacion: result});
+            });
+        });
+
+    }catch(error){
+        console.log(error);
+        return res.status(500).json({error: "contacta con el administrador del sistema"})
+    }
+}
+
+exports.recieve = (req,res) =>{
+    try {
+
+        const { requestId } = req.body;
+        soap.createClient(MESSAGE_URL, (err, client) =>{
+            if(err) throw new Error(err);
+            const data = {
+                requestId: requestId
             };
 
             client.setEndpoint(MESSAGE_URL);
             client.setSecurity(new soap.BasicAuthSecurity( SERVICE_KEY, SERVICE_PASSWORD));
-            client.sendMessage(data, (err, result) =>{
-                if(err) return res.status(400).json({exito: false, informacion: err});
-                if(result.resultCode != "0") return res.status(200).json({exito: false , informacion: data});
-                return res.status(200).json({exito: true , informacion: result});
+            client.getReceivedMessage(data, (err, result) =>{
+                if(err) return res.status(400).json(err);
+                return res.status(200).json(result);
             });
         });
 
